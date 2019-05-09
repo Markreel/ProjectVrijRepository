051123bf9f5ec5568907fcd46b2ace8261f06f2d
Markreel <markmeerssman@live.nl>
2019-05-10 01:04:23 +0200

    Added cooldown to dash Cleaned up functions Changed jumping behaviour

--- ProjectVrij/Assets/Scripts/InputManager.cs
+++ ProjectVrij/Assets/Scripts/InputManager.cs
@@ -4,66 +4,102 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    [Header("Settings: ")]
 	[SerializeField] private float moveSpeed = 5f;
 	[SerializeField] private float jumpHeight = 2f;
+	[SerializeField] private int maxJumpAmount = 1;
 	[SerializeField] private float gravity = -9.81f;
 	[SerializeField] private float groundDistance = 0.2f;
-	[SerializeField] private float dashDistance = 5f;
 	[SerializeField] private LayerMask groundLayer;
+    [Space]
+	[SerializeField] private float dashDistance = 5f;
 	[SerializeField] private Vector3 drag;
-	[SerializeField] private int maxJumpAmount = 1;
+    [SerializeField] private float dashDelay = 1f;
 
 	private int currentJumpAmount = 1;
+    private float currentDashDelay;
 	private CharacterController charController;
 	private Vector3 velocity;
 	private bool isGrounded = true;
 	private Transform groundChecker;
 
 
-	void Start()
+	private void Start()
 	{
 		charController = GetComponent<CharacterController>();
 		groundChecker = transform.GetChild(0);
-	}
+        currentDashDelay = 0;
+    }
 
-	void Update()
+    private void Update()
 	{
-		isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, groundLayer, QueryTriggerInteraction.Ignore);
-
-		//Check Grounded
-		if (isGrounded && velocity.y < 0)
-		{
-			currentJumpAmount = maxJumpAmount;
-			velocity.y = 0f;
-		}
-
-		Vector3 _move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-		charController.Move(_move * Time.deltaTime * moveSpeed);
-
-		if (_move != Vector3.zero)
-		{
-			transform.forward = _move;
-		}
-
-		//Jump With DoubleJump
-		if (Input.GetKeyDown(KeyCode.Space) && currentJumpAmount > 0)
-		{
-			velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
-			currentJumpAmount--;
-		}
-		
-		//Dash
-		if (Input.GetKeyDown(KeyCode.LeftShift))
-		{
-			velocity += Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * drag.x + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * drag.z + 1)) / -Time.deltaTime)));
-		}
-
-		velocity.y += gravity * Time.deltaTime;
-
-		velocity.x /= 1 + drag.x * Time.deltaTime;
-		velocity.y /= 1 + drag.y * Time.deltaTime;
-		velocity.z /= 1 + drag.z * Time.deltaTime;
-
-		charController.Move(velocity * Time.deltaTime);
+        HandleMovement();
 	}
+
+    private void HandleMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundDistance, groundLayer, QueryTriggerInteraction.Ignore);
+
+        CheckIfGrounded();
+        CoolDownDash();
+
+        Vector3 _move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        charController.Move(_move * Time.deltaTime * moveSpeed);
+
+        if (_move != Vector3.zero)
+            transform.forward = _move;
+
+        //Jump With DoubleJump
+        if (Input.GetKeyDown(KeyCode.Space) && currentJumpAmount > 0)
+            Jump();
+
+        //Dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && currentDashDelay <= 0)
+            Dash();
+
+        velocity.y += gravity * Time.deltaTime;
+
+        velocity.x /= 1 + drag.x * Time.deltaTime;
+        velocity.y /= 1 + drag.y * Time.deltaTime;
+
+        charController.Move(velocity * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Launches the player upwards according to the amount of jumps available.
+    /// </summary>
+    private void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        currentJumpAmount--;
+    }
+
+    /// <summary>
+    /// Launches the player sideways towards the current direction.
+    /// </summary>
+    private void Dash()
+    {
+        velocity += Vector3.Scale(transform.forward, dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * drag.x + 1)) / -Time.deltaTime), 0, 0));
+        currentDashDelay = dashDelay;
+    }
+
+    private void CoolDownDash()
+    {
+        if (currentDashDelay > 0)
+            currentDashDelay -= Time.deltaTime;
+        else
+            currentDashDelay = 0;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently touching the ground (not in the air).
+    /// </summary>
+    private void CheckIfGrounded()
+    {
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+            currentJumpAmount = maxJumpAmount;
+        }
+    }
 }
