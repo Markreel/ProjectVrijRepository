3c58753ac44d411fef054428279c989a56e7482e
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-06-14 14:58:36 +0200

    -Added EnumStorage.cs for multiple states to be handled -Changed Player.cs -Updated InputManager.cs
    -Fixed DeathAnimation (Player can now die in peace).
    -Updated PlayerAnimator controller

---
+++ ProjectVrij/Assets/Scripts/EnumStorage.cs
@@ -0,0 +1,27 @@
+﻿public class EnumStorage
+{
+	public enum PlayerState
+	{
+		Idle,
+		Dashing,
+		Moving,
+		Dead
+	}
+
+	public enum BasicEnemyState
+	{
+		Idle,
+		Attacking,
+		Moving,
+		Dead
+	}
+
+	public enum BossEnemyState
+	{
+		Idle,
+		AttackingType1,
+		AttackingType2,
+		Moving,
+		Dead
+	}
+}
